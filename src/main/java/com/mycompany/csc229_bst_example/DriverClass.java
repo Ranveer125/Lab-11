dbb6172e0ca3d625e6c35f8fb0029945b1533ce5
ranveersingh <ranveersingh@ranveers-air.lan>
2023-04-23 09:41:39 -0400

    Create getData field in node class to print node

--- src/main/java/com/mycompany/csc229_bst_example/DriverClass.java
+++ src/main/java/com/mycompany/csc229_bst_example/DriverClass.java
@@ -25,10 +25,47 @@ public class DriverClass {
         bst.preOrderTraversal();
         System.out.println("In Order Traversal");
         bst.inOrderTraversal();
-        System.out.print("\nHeight of the tree: " + bst.findHeight());
-        
+        System.out.print("\nHeight of the tree: " + bst.findHeight());       
         bst.print("",  false);
     }
+    public void inOrderTraversal(){
+        System.out.print("InOrderTraversal: ");    //InOrderTraversal Method
+        System.out.println();                   
+    }
+    private void doInOrder(BstNode root){
+if(root==null){
+}
+doInOrder(root.getLeft());
+System.out.print(root.getData() + " "); // Get Data for Root
+doInOrder(root.getRight());
+}
+    public void preOrderTraversal(){
+        System.out.print("PreOrder Traversal: ");     // PreOrder Traversal Method
+        System.out.println();
+    }
+    private void doPreOrder(BstNode root){
+        if(root==null){
+            return;
+        }             
+        // Add in return statement
+    }
+    public int getDepth(BstNode node, int par){
+        return getDepth(node, 0);
+    }
+    private int getDepth(BstNode root, BstNode node, int depth){
+        if(root==null){
+            return -1;
+        }
+        return 0;
+    }
+public void print(String string, boolean par){
+    print("", false);
+}
+private void print(String prefix, BstNode node, boolean isLeft){
+    if(node != null){
+        System.out.println(node.getData);
+    }
+}
 }
 
 
